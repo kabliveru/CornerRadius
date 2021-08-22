54b4036373524ddac909ebe0ff77560fd985e8eb
kabliveru <kab@live.ru>
2021-08-23 05:15:34 +0530

    Initial Commit

---
+++ Shared/CornerRadiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornerRadiusApp.swift
+//  Shared
+//
+//  Created by  kabliveru on 23.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct CornerRadiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
